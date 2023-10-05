a63c28b00a2c5c02a1d31fa7449e04c3bcc6217b
Stubborn <131737791+stubbornlove@users.noreply.github.com>
2023-10-05 23:27:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6424305558:AAEHxfOFwj8wwjO5Y1B-UGWcqK_CAiXEAek")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6278216192:AAEKQEI3Zkv1V1bMQYKoFcW-M57OVvX3Znk")
     API_ID = int(os.environ.get("API_ID", "22263567"))
     API_HASH = os.environ.get("API_HASH", "258fb6f6cdfa8220b74a4a6753b2ece6")
     AUTH_USERS = "6446062004"
